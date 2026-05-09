426790d75f1bcbf3c065885469b8bc00ecfa1225
Km gnana Deepak <kmgnanadeepak@gmail.com>
2026-05-09 21:18:24 +0530

    Delete kisan directory

--- kisan/backend/src/routes/modules/prescriptionRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { generatePrescriptionController } from '../../controllers/prescriptionController.js';
-
-const router = Router();
-
-router.post('/generate', generatePrescriptionController);
-
-export default router;
-
--- kisan/backend/src/routes/modules/shopRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getNearbyShopsController } from '../../controllers/shopController.js';
-
-const router = Router();
-
-router.get('/nearby', getNearbyShopsController);
-
-export default router;
-
